e61204df4cf2f11cb55a2b2f3ab789a58ad2ab16
kanamikosio <kanami.koiso@is-tech.co.jp>
2024-08-09 18:28:18 +0900

    商品関連

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
@@ -14,7 +14,9 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">
+                        You're logged in!
+                    </div>
                 </div>
             </div>
         </div>
